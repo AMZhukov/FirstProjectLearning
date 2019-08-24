4c812fa22f75c95fec0f87db93101b4c3d6a15db
Andrey Zhukov <a.m.zhukov@inbox.ru>
2019-08-24 23:50:30 +0500

    fourth branch, update commit

--- src/index-two.js
+++ src/index-two.js
@@ -1,13 +1,16 @@
-const number = 266219;
-const currentNumber1 = Array.from(String(number), Number);
-let multiNumber = 1;
+const number = 266219; 
+let currentNumber1 = null,
+  multiNumber = 1,
+  cubingNumber = null,
+  currentNumber2 = null,
+  sumNumber = null;
 
-for(let i = 0; i < currentNumber1.length; i++) {
-    multiNumber *= currentNumber1[i];
+currentNumber1 = Array.from(String(number), Number); // convert numbers to array
+for (let i = 0; i < currentNumber1.length; i += 1) {
+  multiNumber *= currentNumber1[i]; // digitals multiplied by each other
 }
-
-const cubingNumber = multiNumber ** 3;
-const currentNumber2 = Array.from(String(cubingNumber), Number);
-const sumNumber = currentNumber2[0] + currentNumber2[1];
+cubingNumber = multiNumber ** 3; // cubing a number
+currentNumber2 = Array.from(String(cubingNumber), Number); // cut first and second digital
+sumNumber = currentNumber2[0] + currentNumber2[1]; // the sum of two numbers
 
 console.log('Сумма первых двух цифр, числа возведённого в куб, состоящего из произведения цифр 2, 6, 6, 2, 1, 9 =', sumNumber);
