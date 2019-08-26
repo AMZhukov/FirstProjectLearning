e7ea4365c11e600fcaec9e4561d9b4a4da65c75b
Andrey Zhukov <a.m.zhukov@inbox.ru>
2019-08-27 00:20:05 +0500

    third commit, thrid branch

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-/* eslint-disable linebreak-style */
-/* eslint-disable indent */
-/* eslint-disable prefer-const */
-/* eslint-disable no-undef */
-/* eslint-disable no-console */
-/* eslint-disable no-alert */
-/* eslint-disable quotes */
-/* eslint-disable no-unused-vars */
-/* eslint-disable one-var */
-// eslint-disable-next-line one-var
-// eslint-disable-next-line prefer-const
+'use strict'
 let money = 15000,
     income = 'freelance',
     addExpenses = 'Food, Water, Laptop',
@@ -16,16 +6,11 @@ let money = 15000,
     mission = 20000,
     period = 12;
 
-const currentAddExpenses = (addExpenses.toLowerCase());
-const budgetDay = money / 30;
-const balanceBudgetDay = money % 30;
-
 console.log('Тип данных money —', typeof money);
 console.log('Тип данных income —', typeof income);
 console.log('Тип данных deposit —', typeof deposit);
 console.log('Длина строки =', income.length);
 console.log('Период', period, 'месяцев');
-console.log('Цель заработать', mission, 'рублей/долларов/гравен/юани');
-console.log(currentAddExpenses.split(', '));
-console.log('Дневной бюджет =', budgetDay);
-console.log('остаток от деления бюджета =', balanceBudgetDay);
+console.log(addExpenses.toLowerCase().split(', ').join(", "));
+console.log('Дневной бюджет =', money / 30);
+console.log('остаток от деления бюджета =', money % 30);
