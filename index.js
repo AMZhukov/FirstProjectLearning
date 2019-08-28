dbccdc08ede70e31990ea176ce20f27907c72ee1
Andrey Zhukov <a.m.zhukov@inbox.ru>
2019-08-28 21:04:09 +0500

    Lesson 3, third commit

--- index.js
+++ index.js
@@ -4,21 +4,13 @@ let money = prompt("Ваш месячный доход?"),
     addExpenses = prompt("Перечислите возможные расходы за рассчитываемый период через запятую"),
     deposit = confirm("Есть ли у вас депозит в банке?"),
     mission = 25000,
-    period = null,
-    spend = [],
-    descrSpend = [],
-    budgetMonth = null,
-    budgetDay = null;
-
-
-descrSpend[0] = prompt("Какие обязательные ежемесячные расходы у вас есть?");
-spend[0] = prompt("Во сколько это обойдется?");
-descrSpend[1] = prompt("Какие обязательные ежемесячные расходы у вас есть?");
-spend[1] = prompt("Во сколько это обойдется?");
-
-budgetMonth = money - (+spend[0]) - (+spend[1]);
-period = Math.ceil(mission / budgetMonth);
-budgetDay = Math.floor(budgetMonth / 30);
+    descrSpend0 = prompt("Какие обязательные ежемесячные расходы у вас есть?"),
+    spend0 = prompt("Во сколько это обойдется?"),    
+    descrSpend1 = prompt("Какие обязательные ежемесячные расходы у вас есть?"),
+    spend1 = prompt("Во сколько это обойдется?"),    
+    budgetMonth = money - (+spend0) - (+spend1),
+    budgetDay = Math.floor(budgetMonth / 30),
+    period = Math.ceil(mission / budgetMonth);
 
 console.log("addExpenses:", addExpenses.split(', '));
 console.log('Тип данных money —', typeof money);
@@ -29,6 +21,6 @@ console.log("Период для накопления", period);
 console.log('Дневной бюджет =', budgetDay);
 
 if (budgetDay >= 800) console.log("Высокий уровень дохода");
-else if ((budgetDay >= 300) || (budgetDay < 800)) console.log("Средний уровень дохода");
-else if ((budgetDay >= 0) || (budgetDay < 300)) console.log("Низкий уровень дохода");
+else if ((budgetDay >= 300) && (budgetDay < 800)) console.log("Средний уровень дохода");
+else if ((budgetDay >= 0) && (budgetDay < 300)) console.log("Низкий уровень дохода");
 else console.log("Низкий уровень дохода");
