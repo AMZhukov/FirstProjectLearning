90e22913500b197fa709d5278df28e69473b9bf6
Andrey Zhukov <a.m.zhukov@inbox.ru>
2019-08-31 18:25:48 +0500

    lesson 5, third commit

--- index.js
+++ index.js
@@ -6,23 +6,11 @@ let money = null,
     mission = 50000,
     pediod = 3;
 
-/*function start (temp0, temp1) {
-    let ask = null;
-    do {
-        ask = prompt(temp0, temp1);
-    } while(isNaN(ask) || ask === '' || ask === null);
-    return +ask;
-}*/
-
-
-
 let start = function() {
     do {
         money = prompt('Ваш месячный доход', 50000);
-    } while(isNaN(money) || money === '' || money === null){
-        console.log('money: ', money);
-    }
-}
+    } while(isNaN(money) || money === '' || money === null);
+};
 start();
 
 
@@ -40,15 +28,17 @@ console.log(addExpenses.toLowerCase().split(','));
 let getExpensesMonth = function(){
     let sum = 0;
     
-    for (let i = 0; i < 2; i++){
+    for (let i = 0, ask = null; i < 2; i++){
         if (i === 0){
             expenses1 = prompt('Введите обязательную статью расходов?', 'Кварплата');
         }
         if (i === 1){
             expenses2 = prompt('Введите обязательную статью расходов?', 'Бензин');
         }
-
-        sum += +prompt('Во сколько это обойдется?', 2500);
+        do{
+            ask = prompt('Во сколько это обойдется?', 2500)
+        } while (isNaN(ask) || ask === '' || ask === null);
+        sum += +ask;
     }
     return sum;
 };
@@ -66,7 +56,7 @@ let getTargetMonth = function (){
 };
 
 let budgetDay = getAccumulatedMonth() / 30,
-    
+
     checkTargetMonth = function () {
     if (getAccumulatedMonth() >= 0){
         return ("Цель будет достигнута за " + Math.ceil(getTargetMonth()) + ' месяца');
