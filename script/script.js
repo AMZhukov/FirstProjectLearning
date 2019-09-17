229397d3d873374a4cf7187a4bccd1c1641b480a
Andrey Zhukov <zhukov290791@gmail.com>
2019-09-17 16:53:13 +0500

    lesson 11, last last

--- script/script.js
+++ script/script.js
@@ -105,7 +105,7 @@ AppData.prototype.addBlock = function(items, btnAdd, type) {
     }
 };
 AppData.prototype.getExpenses = function () {
-    expensesItems = document.querySelectorAll('.expenses-item');
+    expensesItems = document.querySelectorAll('.expenses-items');
     expensesItems.forEach( (item) => {
         let itemExpenses = item.querySelector('.expenses-title').value;
         let cashExpenses = item.querySelector('.expenses-amount').value;
@@ -116,7 +116,7 @@ AppData.prototype.getExpenses = function () {
 };
 
 AppData.prototype.getIncome = function () {
-    incomeItems = document.querySelectorAll('.income-item');
+    incomeItems = document.querySelectorAll('.income-items');
     incomeItems.forEach( (item) => {
         let itemIncome = item.querySelector('.income-title').value;
         let cashIncome = item.querySelector('.income-amount').value;
